f553ba20796f466b4e4479d80648763989693fba
Mercandj <modjow.jm9@gmail.com>
2022-05-02 12:17:42 +0200

    [AndroidStudio] File template fixes

--- android_studio_settings/fileTemplates/Kotlin Screen Package.kt
+++ android_studio_settings/fileTemplates/Kotlin Screen Package.kt
@@ -7,17 +7,18 @@ import android.widget.FrameLayout
 import androidx.annotation.IdRes
 import ${PACKAGE_NAME}.R
 import com.mercandalli.android.sdk.view.ViewExtension.inflateLayout
+import com.mercandalli.android.sdk.view.ViewUtils.visibleToVisibleOrGone
 
 #end
 #parse("File Header.java")
 #set ($FEATURE_CAMEL_CASE = ${StringUtils.removeAndHump(${NAME}, "_")})
-class ${NAME} @JvmOverloads constructor(
+class ${FEATURE_CAMEL_CASE} @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : FrameLayout(context, attrs, defStyleAttr) {
 
-    private val view = inflateLayout(R.layout.${FEATURE})
+    private val view = inflateLayout(R.layout.${NAME})
     private val userAction by lazy { createUserAction() }
     
     override fun onAttachedToWindow() {
@@ -35,22 +36,22 @@ class ${NAME} @JvmOverloads constructor(
         return view.findViewById<T>(id)
     }
     
-    private fun createScreen() = object : ${NAME}Contract.Screen {
+    private fun createScreen() = object : ${FEATURE_CAMEL_CASE}Contract.Screen {
         override fun setVisibility(visible: Boolean) {
             visibility = visible.visibleToVisibleOrGone()
         }
     }
     
-    private fun createUserAction(): ${NAME}Contract.UserAction {
+    private fun createUserAction(): ${FEATURE_CAMEL_CASE}Contract.UserAction {
         if (isInEditMode) {
-            return object : ${NAME}Contract.UserAction {
+            return object : ${FEATURE_CAMEL_CASE}Contract.UserAction {
                 override fun onAttachedToWindow() {}
                 override fun onDetachedFromWindow() {}
             }
         }
-        return ${NAME}Presenter(
+        return ${FEATURE_CAMEL_CASE}Presenter(
             createScreen(),
-            Graph.get${NAME}Manager()
+            Graph.get${FEATURE_CAMEL_CASE}Manager()
         )
     }
 }
--- android_studio_settings/fileTemplates/Kotlin Screen Package.kt.child.4.kt
+++ android_studio_settings/fileTemplates/Kotlin Screen Package.kt.child.4.kt
@@ -3,20 +3,23 @@
 #end
 #parse("File Header.java")
 #set ($FEATURE_CAMEL_CASE = ${StringUtils.removeAndHump(${NAME}, "_")})
+#set ($FEATURE_CAMEL_CASE_FIRST_LETTER = $FEATURE_CAMEL_CASE.substring(0, 1).toLowerCase())
+#set ($FEATURE_CAMEL_CASE_REST = $FEATURE_CAMEL_CASE.substring(1))
+#set ($FEATURE_CAMEL_CASE_UNCAPITALIZED = ${FEATURE_CAMEL_CASE_FIRST_LETTER} + ${FEATURE_CAMEL_CASE_REST})
 class ${FEATURE_CAMEL_CASE}Presenter(
     private val screen: ${FEATURE_CAMEL_CASE}Contract.Screen,
-    private val m${FEATURE_CAMEL_CASE}Manager: ${FEATURE_CAMEL_CASE}Manager
+    private val ${FEATURE_CAMEL_CASE_UNCAPITALIZED}Manager: ${FEATURE_CAMEL_CASE}Manager
 ) : ${FEATURE_CAMEL_CASE}Contract.UserAction {
 
-    private val m${FEATURE_CAMEL_CASE}Listener = create${FEATURE_CAMEL_CASE}Listener()
+    private val ${FEATURE_CAMEL_CASE_UNCAPITALIZED}Listener = create${FEATURE_CAMEL_CASE}Listener()
 
     override fun onAttachedToWindow() {
-        m${FEATURE_CAMEL_CASE}Manager.addListener(m${FEATURE_CAMEL_CASE}Listener)
+        ${FEATURE_CAMEL_CASE_UNCAPITALIZED}Manager.addListener(${FEATURE_CAMEL_CASE_UNCAPITALIZED}Listener)
         updateScreen()
     }
 
     override fun onDetachedFromWindow() {
-        m${FEATURE_CAMEL_CASE}Manager.removeListener(m${FEATURE_CAMEL_CASE}Listener)
+        ${FEATURE_CAMEL_CASE_UNCAPITALIZED}Manager.removeListener(${FEATURE_CAMEL_CASE_UNCAPITALIZED}Listener)
     }
 
     private fun updateScreen() {
@@ -28,7 +31,7 @@ class ${FEATURE_CAMEL_CASE}Presenter(
     }
  
     private fun createVisibility(): Boolean {
-        return m${FEATURE_CAMEL_CASE}Manager.getViewModel() != null
+        return ${FEATURE_CAMEL_CASE_UNCAPITALIZED}Manager.getViewModel() != null
     }
     
     private fun create${FEATURE_CAMEL_CASE}Listener() = object: ${FEATURE_CAMEL_CASE}Manager.Listener {
